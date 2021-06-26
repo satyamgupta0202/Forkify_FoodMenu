ad6e1cf0d9a9a294dfb69b996969ababa8a258d0
satyam <2019335@iiitdmj.ac.in>
2021-06-26 12:20:25 +0530

    Forkify Project

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '59ae3352-7037-453a-b5a5-b32ea541a237';
+export const MODAL_CLOSE_SEC = 2.5;
